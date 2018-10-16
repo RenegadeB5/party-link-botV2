fed7499cbccece5d1bccadd3cdbce7774fdc93a6
RenegadeB5 <rocketmanB5@gmail.com>
2018-10-16 15:10:43 -0400

    Create linkunban.js

---
+++ commands/moderation/linkunban.js
@@ -0,0 +1,26 @@
+var { Command } = require("discord.js-commando");
+module.exports = class PingCommand extends Command { 
+	constructor(client) {super(client, {
+		name: "linkunban",
+		group: "moderation",
+		description: "Prevents a user from accessing member-links.",
+		memberName: "linkunban",
+		args: [
+ 			{
+				key: 'user',
+				prompt: 'The user to un-linkban',
+				type: 'user'
+			}
+		]
+	});
+						}
+
+	async run(msg,user) {
+		let banrole = msg.guild.roles.get('483870035077758976');
+		if(!msg.member.roles.has(banrole.id)) return msg.channel.send("The specified user isnt linkbanned.");
+		msg.member.removeRole(banrole).then(user.send('Your access to member-links has been restored'));
+	}
+}
+exports.help = {
+	name: "linkunban"
+}
