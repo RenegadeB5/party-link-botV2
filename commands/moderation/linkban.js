60a61684a4b3fe42ac44ff339adc5f8b32c39f54
RenegadeB5 <rocketmanB5@gmail.com>
2018-10-16 17:21:24 -0400

    Update linkban.js

--- commands/moderation/linkban.js
+++ commands/moderation/linkban.js
@@ -16,10 +16,10 @@ module.exports = class PingCommand extends Command {
 						}
 
 	async run(msg,user) {
+		msg.delete();
 		let banrole = msg.guild.roles.get('501861061364940800');
 		if(msg.member.roles.has(banrole.id)) return msg.channel.send("The specified user is already linkbanned!").then(message => {message.delete(5000)});
 		msg.member.addRole(banrole).then(msg.mentions.members.first().send('You have been banned from accessing member-links! The only way the punishment will be lifted is if you sincerely apologize to a staff member.'));
-		msg.delete();
 	}
 }
 exports.help = {
