fb4630e680e1767a0b364151b5c328dd9d3fbcfd
RenegadeB5 <rocketmanB5@gmail.com>
2018-10-17 12:04:14 -0400

    Update info.js

--- commands/basics/info.js
+++ commands/basics/info.js
@@ -3,13 +3,13 @@ const Discord = require('discord.js');
 exports.run = async (client, msg, args) => {
 
     let botembed = new Discord.RichEmbed()
-        .setDescription("Information on Sapphyr")
+        .setDescription("Information on Party Link Bot")
         .setThumbnail(client.user.displayAvatarURL)
         .addField("Bot Name", client.user.username, true)
         .addField("Created On", client.user.createdAt, true)
         .addField("Total Users", client.users.size, true)
         .addField("Total Guilds", client.guilds.size, true)
-        .addField("Developer(s)", "Nef#8443, rjt#2336", true);
+        .addField("Developer(s)", "Nef#8443, rjt#2336, RenegadeBB#4311", true);
 
     msg.channel.send(botembed);
 }
